fd840454521c7b88b152e1b4ca6aad9822ce28ba
coolencyclopedia <19parikshitjain@gmail.com>
2026-04-17 00:33:50 +0530

    second commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,8 @@
+# Load modular PowerShell profile files
+. "$HOME\PowerShell\ascii-logo.ps1"
+. "$HOME\PowerShell\posh-init.ps1"
+. "$HOME\PowerShell\modules.ps1"
+. "$HOME\PowerShell\aliases.ps1"
+. "$HOME\PowerShell\functions.ps1"
+. "$HOME\PowerShell\terminalcolor.ps1"
+. "$HOME\PowerShell\fzf_file.ps1"
